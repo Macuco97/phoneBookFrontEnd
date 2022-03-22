c179acfdc31b6be0a891692f0ca65d6869d6e250
Raphae <raphae@pop-os.localdomain>
2022-03-22 14:46:20 -0300

    Fixing error React must be in scope when using JSXX react/react-in-jsx-scope

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Axios from "axios"
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 
 function App() {
   const [database, setDatabase] = useState()
