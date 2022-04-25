5d3e200234cb9617fc8c813355728cf547767114
Raphae <raphae@pop-os.localdomain>
2022-04-24 22:30:30 -0300

    Fixing bug in width input

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
   const [pictureChosen, setPictureChosen] = useState()
   const [allowForDeleteAlert, setAllowForDeleteAlert] = useState()
   const [fieldEmptyAlert, setFieldEmptyAlert] = useState()
+  const [cardEmptyAlert, setCardEmptyAlert] = useState()
   const updateInput = useRef()
   const dataBaseUrl = "http://localhost:3001"
   
@@ -64,12 +65,11 @@ function App() {
         .catch(err => console.log(err))
     }
     else {
-      alert("PLEASE, FILL ALL INPUTS")
+      setFieldEmptyAlert(true)
     }
       
   
   }
-
 
   const deleteNewUser = () => {
       if(cardId) {
@@ -88,11 +88,8 @@ function App() {
           .catch(err => console.log(err))
       }
       else{
-
-      }
-     
-    
-      
+        setCardEmptyAlert(true)
+      }  
   }
 
   const updateNewUser = () => {
@@ -158,6 +155,16 @@ function App() {
     setEditMode(!editMode)
   },[dataBaseRows])
 
+  useEffect (() => {
+    if(fieldEmptyAlert || cardEmptyAlert) {
+      setTimeout(() => {
+        setFieldEmptyAlert(false)
+        setCardEmptyAlert(false)
+      }, 10000)
+    }
+    
+  },[allowForDeleteAlert, fieldEmptyAlert, cardEmptyAlert])
+
   return (
     <>
     <div className = 'container-fluid d-flex bg-dark'>
@@ -168,7 +175,7 @@ function App() {
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
               <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
             </svg>
-              <span className = 'flex-grow-1'>PLEASE, FILL ALL INPUTS !</span>
+              <span className = 'flex-grow-1'>Fill all inputs !</span>
               <button 
               type = 'button' 
               class = {`ms-3 btn btn-close align-self-center`} 
@@ -178,9 +185,27 @@ function App() {
               />
         </div>
       </div>  
-      } 
+      }
+      {
+        cardEmptyAlert && 
+        <div className = {`fixed-top justify-content-center d-flex`}>
+        <div class="m-1 alert alert-warning  p-2  mx-3 w-50 d-flex"  role="alert">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+              <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </svg>
+              <span className = 'flex-grow-1'>Select some card clicking there </span>
+              <button 
+              type = 'button' 
+              class = {`ms-3 btn btn-close align-self-center`} 
+              aria-label = {`Close`} 
+              style = {{position: "relative", right: "0"}}
+              onClick = {() => setCardEmptyAlert(false)}
+              />
+        </div>
+      </div>  
+      }
 				<div className = 'row min-vh-100'>
-          <div className = {`py-4 col-4 min-vh-100 bg-secondary `}> 
+          <div className = {`py-4 col-3 min-vh-100 bg-secondary `}> 
             <form onSubmit = {e => createNewUser(e)}>
               <h5>Create New User</h5>
               <div className = 'input-group my-3'>
